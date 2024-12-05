2b019cb65817946acff0006b1863fb85618dcc20
SrikariAmrutham <122285521+SrikariAmrutham@users.noreply.github.com>
2024-12-06 00:28:40 +0530

    Save code for question: Rotate Image

---
+++ Rotate_Image.java
@@ -0,0 +1,41 @@
+
+  // Question: Rotate Image
+  // Description: You are given an n x n 2D matrix representing an image, rotate the image by 90 degrees (clockwise).
+  
+  import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt(); 
+        
+        int[][] matrix = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+
+        rotateMatrix(matrix);
+        for (int[] row : matrix) {
+            for (int num : row) {
+                System.out.print(num + " ");
+            }
+            System.out.println();
+        }
+    }
+    public static void rotateMatrix(int[][] matrix) {
+        int n = matrix.length;
+        for (int i = 0; i < n / 2; i++) {
+            for (int j = i; j < n - i - 1; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[n - j - 1][i];
+                matrix[n - j - 1][i] = matrix[n - i - 1][n - j - 1];
+                matrix[n - i - 1][n - j - 1] = matrix[j][n - i - 1];
+                matrix[j][n - i - 1] = temp;
+            }
+        }
+    }
+}
+
+  
